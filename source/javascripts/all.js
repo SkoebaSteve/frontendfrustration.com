d3c0994f171eb3dd653ae7717d983727233ac62c
SkoebaSteve <steef@hhd.com.au>
2016-09-05 19:31:50 +0200

    ready for build

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,13 @@
 //= require turbolinks
 //= require_tree .
 
+document.addEventListener("turbolinks:click", function(event) { 
+  //do work
+  console.log("link clicked");
+  // document.classList.add("fade");
+});
+
+document.addEventListener("turbolinks:load", function(event) { 
+  //do work
+  console.log("page load");
+});
